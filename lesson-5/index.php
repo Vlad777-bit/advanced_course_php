0f0dcdbc28ee12fffd7296784b42b14e25b7c6d1
Vladislav <mega.nozhkin@mail.ru>
2021-08-26 16:35:00 +0300

    added template

---
+++ lesson-5/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('./views');
+$twig = new \Twig\Environment($loader, []);
+
+$template = $twig->load('v_main.html');
+
+$titlePage = 'Регистация';
+
+
+echo $template->render([
+  'titlePage' => $titlePage,
+]);
